4385dd867f4280fac9b199ae3557608b1b2cd55c
Shawn Weisfeld <shawn@shawnweisfeld.com>
2019-04-19 13:42:18 -0500

    added load controller

---
+++ src/Controllers/LoadController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace todo.Controllers
+{
+    public class LoadController : Controller
+    {
+        // GET: Error
+        public ActionResult Index(int duration)
+        {
+            var random = new Random();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            while (watch.ElapsedMilliseconds < duration)
+            {
+                var x = random.NextDouble();
+                var y = random.NextDouble();
+                var z = x * y;
+            }
+
+            return View();
+        }
+        
+    }
+}
